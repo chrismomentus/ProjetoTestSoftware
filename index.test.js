929df217056adf72fe79fc915e7314141d3f480b
Chris Chinedozie <chris.chinedozie@cloudwalk.io>
2022-05-04 15:50:01 -0300

    test added for triangle function

--- index.test.js
+++ index.test.js
@@ -2,9 +2,11 @@ const index = require('./index');
 
 const lista = [[3,3,3,"equilatero"], [5,5,5, "equilatero"], [10,10,12,"isosceles"], [6,6,8,"isosceles"], [5,5,6,"isosceles"],
                  [7,9,14,"escaleno"], [0,0,0,"invalido"], [0,0,1,"invalido"], [0,1,1,"invalido"], [5,3,9,"invalido"], [3,9,5,"invalido"],
-                 [9,3,5,"invalido"], [5,5,10, "invalido"]]
+                 [9,3,5,"invalido"], [5,5,10, "invalido"]];
 
 
+
+        
 
   for (let i = 0; i < lista.length; i++) {
     const element = lista[i];
@@ -20,12 +22,10 @@ const lista = [[3,3,3,"equilatero"], [5,5,5, "equilatero"], [10,10,12,"isosceles
     });
   }
 
-  for (let i = 0; i < lista.length; i++) {
-    const element = lista[i];
-    test('Run Triangulo3', () => {
-      expect(index.TipoTriangulo3(element[0],element[1],element[2])).toBe(element[3]);    
-    });
-  }
+
+  test.each(lista)('Run Triangulo3', () => {
+    expect(index.TipoTriangulo3(element[0],element[1],element[2])).toBe(element[3]);    
+  });
 
 // # Testes 1
 // # Esperando Equilátero
