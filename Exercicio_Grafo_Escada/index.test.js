d387950da6727f492572c71736c0ba98e799ea68
chrismomentus <chrismomentus@gmail.com>
2022-07-26 22:34:20 -0300

    added updates on mutation of escada code test

---
+++ Exercicio_Grafo_Escada/index.test.js
@@ -0,0 +1,23 @@
+/*
+    Utilizando a biblioteca JEST.
+    Testar o codigo escada3
+*/
+
+const index = require('./index');
+
+ let N = 4
+ let M = 6
+
+ var matriz =   [[4, 6, [[1, 2, 9, 9, 9, 9],[0, 1, 2, 3, 4, 5],[0, 0, 1, 2, 3, 4],[0, 0, 0, 1, 2, 3]], "S"],
+                [5, 5, [[1, 2, 9, 9, 9],[0, 1, 2, 3, 4],[0, 1, 1, 2, 3],[0, 0, 0, 1, 0], [0, 0, 0, 0, 0]], "N"],
+                [5, 8, [[0, 5, 9, 9, 9, 2, 2, 0],[0, 0, 0, 0, 4, 0, 1, 2],[0, 0, 0, 0, 0, 0, 3, 2],[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], "N"] ];
+
+const reorderList = matriz.map(item => {
+    return [item[0], item[1], item[2], item[3]];
+});
+
+//console.log(reorderList);
+
+test.each(reorderList)('Should Run Escada3 with matrix dimension of \"%s\" \"%s\" and matrix \"%s\" should result to \"%s\"',(N, M, matriz, expectedResult) =>{
+    expect(index.escada3(N,M, matriz)).toStrictEqual(expectedResult);
+});
